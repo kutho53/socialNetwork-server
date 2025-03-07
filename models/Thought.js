645cfe4fec36482925d27cebffdc9f50a1dea61e
kutho53 <kurtisthomas53@gmail.com>
2025-03-07 18:23:50 -0500

    added models and starter folders

---
+++ models/Thought.js
@@ -0,0 +1,64 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReachtionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: 'Provide a reachtion',
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: 'Provide Username'
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true,
+        }
+    }
+);
+
+const ThoughtSchema = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: 'Must provide username'
+        },
+        reactions: [ReachtionSchema]
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true
+        }
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function(){
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+module.exports = Thought;
